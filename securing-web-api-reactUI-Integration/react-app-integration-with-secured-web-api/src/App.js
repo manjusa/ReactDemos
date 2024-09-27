ee582260233c3efed2d7e68eaf02f4c90fc74693
manjusa <thizismanju@mail.com>
2024-09-27 12:27:51 +1000

    initial draft

---
+++ securing-web-api-reactUI-Integration/react-app-integration-with-secured-web-api/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { useIsAuthenticated } from "@azure/msal-react";
+import { Login } from "./Login";
+function App() {
+  const isAuthenticated = useIsAuthenticated();
+  debugger;
+  return (
+    <div>
+      {isAuthenticated ? (
+        <h1>Welcome to our application. You are logged in</h1>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+}
+
+export default App;
