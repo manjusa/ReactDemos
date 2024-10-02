c68cfeb8a6f094f73a547266e5c8a003be07c4ce
manjusa <thizismanju@mail.com>
2024-10-02 17:32:31 +1000

    commiting api changes

--- securing-web-api-reactUI-Integration/react-app-integration-with-secured-web-api/src/App.js
+++ securing-web-api-reactUI-Integration/react-app-integration-with-secured-web-api/src/App.js
@@ -1,9 +1,47 @@
 import "./App.css";
 import { useIsAuthenticated } from "@azure/msal-react";
 import { Login } from "./Login";
+import { useEffect, useState } from "react";
+import { useMsal } from "@azure/msal-react";
+import { loginRequest } from "./authConfig";
 function App() {
   const isAuthenticated = useIsAuthenticated();
-  debugger;
+  const { instance, accounts } = useMsal();
+  const [authToken, setAuthToken] = useState();
+
+  useEffect(() => {
+    if (authToken) {
+      fetch("https://localhost:7297/Weatherforecast", {
+        headers: {
+          Authorization: "Bearer " + authToken,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Handle the response data here
+          console.log(data);
+        })
+        .catch((error) => {
+          // Handle any errors here
+          debugger;
+          console.error(error);
+        });
+    }
+  }, [authToken]);
+
+  useEffect(() => {
+    if (accounts.length > 0) {
+      instance
+        .acquireTokenSilent({
+          ...loginRequest,
+          account: accounts[0],
+        })
+        .then((response) => {
+          setAuthToken(response.accessToken);
+        });
+    }
+  }, [accounts, instance]);
+
   return (
     <div>
       {isAuthenticated ? (
